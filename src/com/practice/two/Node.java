ac96de973d6d551d2e26d6103b0a184862a610e1
John Gibbs <hello@johnhenrygibbs.com>
2024-01-27 16:45:16 -0800

    Linked Lists

---
+++ src/com/practice/two/Node.java
@@ -0,0 +1,14 @@
+package com.practice.two;
+
+public class Node {
+
+    Node next = null;
+    int data;
+
+    public Node (int d) {
+
+        data = d;
+
+    }
+
+}
---
+++ src/com/practice/two/RemoveDuplicates.java
@@ -0,0 +1,37 @@
+package com.practice.two;
+
+// Write code to remove duplicates from an unsorted linked list.
+
+import java.util.HashSet;
+
+public class RemoveDuplicates {
+
+    void deleteDupes(Node n) {
+
+        HashSet<Integer> set = new HashSet<Integer>();
+        Node previous = null;
+
+        while (n != null) {
+
+            if (set.contains(n.data)) {
+
+                previous.next = n.next;
+
+            } else {
+
+                set.add(n.data);
+                previous = n;
+
+            }
+
+            n = n.next;
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
