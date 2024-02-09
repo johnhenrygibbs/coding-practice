7a5ff82abd902e823960c9982fc1e5e4d443e69d
John Gibbs <hello@johnhenrygibbs.com>
2024-02-08 23:20:13 -0800

    Linked Lists

--- src/com/practice/two/Palindrome.java
+++ src/com/practice/two/Palindrome.java
@@ -2,6 +2,8 @@ package com.practice.two;
 
 // Implement a function to check if a linked list is a palindrome.
 
+import java.util.Stack;
+
 public class Palindrome {
 
     // One solution is to reverse the linked list and compare the two lists against one another.
@@ -47,6 +49,47 @@ public class Palindrome {
         }
 
         return one == null && two == null;
+
+    }
+
+    // Another solution is an iterative approach.
+
+    boolean isIterativePalindrome(MyNode head) {
+
+        MyNode fast = head;
+        MyNode slow = head;
+
+        Stack<Integer> stack = new Stack<Integer>();
+
+        while (fast != null && fast.next != null) {
+
+            stack.push(slow.data);
+            slow = slow.next;
+            fast = fast.next.next;
+
+        }
+
+        if (fast != null) {
+
+            slow = slow.next;
+
+        }
+
+        while (slow != null) {
+
+            int top = stack.pop().intValue();
+
+            if (top != slow.data) {
+
+                return false;
+
+            }
+
+            slow = slow.next;
+
+        }
+
+        return true;
 
     }
 
