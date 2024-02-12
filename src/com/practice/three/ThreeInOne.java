f69e8e6f4548042e06965d70ded5248a8b9a5036
John Gibbs <hello@johnhenrygibbs.com>
2024-02-11 22:47:16 -0800

    Stacks and Queues

--- src/com/practice/three/ThreeInOne.java
+++ src/com/practice/three/ThreeInOne.java
@@ -4,6 +4,21 @@ package com.practice.three;
 
 public class ThreeInOne {
 
+    class FixedMultiStack {
 
+        private int numberOfStacks = 3;
+        private int stackCapacity;
+        private int[] values;
+        private int[] sizes;
+
+        public FixedMultiStack(int stackSize) {
+
+            stackCapacity = stackSize;
+            values = new int[stackSize * numberOfStacks];
+            sizes = new int[numberOfStacks];
+
+        }
+
+    }
 
 }
