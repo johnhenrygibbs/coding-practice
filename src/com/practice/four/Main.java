db0969e45dd8f2648149063baf7bd9087db3156a
John Gibbs <hello@johnhenrygibbs.com>
2024-02-27 19:16:51 -0800

    Tree and Graphs

---
+++ src/com/practice/four/Main.java
@@ -0,0 +1,7 @@
+package com.practice.four;
+
+public class Main {
+
+
+
+}
---
+++ src/com/practice/four/TreeNode.java
@@ -0,0 +1,28 @@
+package com.practice.four;
+
+public class TreeNode {
+
+    class Node {
+
+        public String name;
+        public TreeNode[] children;
+
+    }
+
+    void inOrderTraversal(TreeNode node) {
+
+        if (node != null) {
+
+            // inOrderTraversal(node.left)
+
+        }
+
+    }
+
+    void visit(TreeNode node) {
+
+        System.out.println(node);
+
+    }
+
+}
