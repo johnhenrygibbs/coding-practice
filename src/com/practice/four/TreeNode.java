f85068eba158c3b0345cb72d83dbb0dde1cc5b2e
John Gibbs <hello@johnhenrygibbs.com>
2024-02-28 13:53:39 -0800

    Trees and Graphs

--- src/com/practice/four/TreeNode.java
+++ src/com/practice/four/TreeNode.java
@@ -2,26 +2,57 @@ package com.practice.four;
 
 public class TreeNode {
 
-    class Node {
+    public String name;
+    public TreeNode left;
+    public TreeNode right;
 
-        public String name;
-        public TreeNode[] children;
+    public TreeNode(String name, TreeNode left, TreeNode right) {
+
+        this.name = name;
+        this.left = left;
+        this.right = right;
 
     }
+
+    // In-order traversal means to "visit" (often, print) the left branch, then the current node, and finally, the right branch.
 
     void inOrderTraversal(TreeNode node) {
 
         if (node != null) {
 
-            // inOrderTraversal(node.left)
+             inOrderTraversal(node.left);
+             System.out.println(node);
+             inOrderTraversal(node.right);
 
         }
 
     }
 
-    void visit(TreeNode node) {
+    // Pre-order traversal visits the current node before its child nodes (hence the name "pre-order").
 
-        System.out.println(node);
+    void preOrderTraversal(TreeNode node) {
+
+        if (node != null) {
+
+            System.out.println(node);
+            preOrderTraversal(node.left);
+            preOrderTraversal(node.right);
+
+        }
+
+    }
+
+    // Post-order traversal visits the current node after its child nodes (hence the name "post-order").
+
+    void postOrderTraversal(TreeNode node) {
+
+        if (node != null) {
+
+            postOrderTraversal(node.left);
+            postOrderTraversal(node.right);
+            System.out.println(node);
+
+        }
 
     }
 
