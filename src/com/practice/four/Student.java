b5e3d982c9ac746bd5a2744876b76c759b3ff20b
John Gibbs <hello@johnhenrygibbs.com>
2024-03-04 17:27:24 -0800

    OOP | 1 Lecture

---
+++ src/com/practice/four/Student.java
@@ -0,0 +1,11 @@
+package com.practice.four;
+
+// OOP 1 | Introduction & Concepts - Classes, Objects, Constructors, Keywords
+
+public class Student {
+
+    String name;
+    int number;
+    int grade;
+
+}
