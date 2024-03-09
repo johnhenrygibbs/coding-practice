6830bf03acef8388f015d5b1e7cfddfc47dc6815
John Gibbs <hello@johnhenrygibbs.com>
2024-03-08 21:59:13 -0800

    OOP 2 | Lecture

--- src/com/practice/oop/Main.java
+++ src/com/practice/oop/Main.java
@@ -6,6 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Human john = new Human(34, "John", 100000, true);
+        Human matt = new Human (39, "Matt", 100000, true);
+
+        System.out.println(Human.population);
+
         Student one = new Student();
         Student two = one;
 
