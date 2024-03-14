acff3fc046ed84e992b3c95bbdf4e8b3ca7edb25
John Gibbs <hello@johnhenrygibbs.com>
2024-03-13 17:59:19 -0700

    Super Keyword

--- src/com/practice/oop/WeightedBox.java
+++ src/com/practice/oop/WeightedBox.java
@@ -10,6 +10,15 @@ public class WeightedBox extends Box {
 
     }
 
+    // Exactly like the "box5" example in Main.
+
+    public WeightedBox(WeightedBox other) {
+
+        super(other);
+        weight = other.weight;
+
+    }
+
     public WeightedBox(double length, double width, double height, double weight) {
 
         super(length, width, height); // Calls the parent class constructor.
