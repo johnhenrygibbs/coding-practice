3d75b6dd702eb68be499566ef898442bb527131f
John Gibbs <hello@johnhenrygibbs.com>
2024-03-16 22:13:22 -0700

    Static Polymorphism

--- src/com/practice/oop/polymorphism/Main.java
+++ src/com/practice/oop/polymorphism/Main.java
@@ -2,6 +2,12 @@ package com.practice.oop.polymorphism;
 
 // Polymorphism is the act of representing the same thing in multiple ways.
 
+// Types of Polymorphism
+
+// 1. Compile time or static polymorphism (achieved via method overloading).
+
+// 2.
+
 public class Main {
 
     // In this example, we have more than one area() method in the package.
