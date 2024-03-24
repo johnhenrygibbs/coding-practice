9227be8517351bfbbb0c779d457a8f06c982bb98
John Gibbs <hello@johnhenrygibbs.com>
2024-03-23 21:57:21 -0500

    equals() Method

--- src/com/practice/oop/access/ObjectDemo.java
+++ src/com/practice/oop/access/ObjectDemo.java
@@ -3,10 +3,12 @@ package com.practice.oop.access;
 public class ObjectDemo {
 
     int num;
+    float gpa;
 
-    public ObjectDemo(int num) {
+    public ObjectDemo(int num, float gpa) {
 
         this.num = num;
+        this.gpa = gpa;
 
     }
 
@@ -22,14 +24,26 @@ public class ObjectDemo {
     @Override
     public boolean equals(Object obj) {
 
-        return super.equals(obj);
+        return this.num == ((ObjectDemo)obj).num;
 
     }
 
     public static void main(String[] args) {
 
-        ObjectDemo obj1 = new ObjectDemo(34);
-        ObjectDemo obj2 = new ObjectDemo(34);
+        ObjectDemo obj1 = new ObjectDemo(34, 56.8f);
+        ObjectDemo obj2 = new ObjectDemo(34, 78.9f);
+
+        if (obj1 == obj2) {
+
+            System.out.println("Object one is equal to object two.");
+
+        }
+
+        if (obj1.equals(obj2)) {
+
+            System.out.println("Object one is equal to object two.");
+
+        }
 
         System.out.println(obj1.hashCode());
         System.out.println(obj2.hashCode());
