b056f01d32cbb71732b2cbcd546959055af658c9
John Gibbs <hello@johnhenrygibbs.com>
2024-04-02 21:48:10 -0700

    Exception Handling Keywords

---
+++ src/com/practice/oop/exceptions/Demo.java
@@ -0,0 +1,11 @@
+package com.practice.oop.exceptions;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        Main.divide(3, 0);
+
+    }
+
+}
--- src/com/practice/oop/exceptions/Main.java
+++ src/com/practice/oop/exceptions/Main.java
@@ -6,16 +6,40 @@ public class Main {
 
         int a = 5;
         int b = 0;
+
         try {
 
-            int c = a / b;
+            divide(a, b);
+
+            throw new Exception ("Just for fun.");
+
+        // Add stricter exception catches first. In this case, subclasses before classes.
 
         } catch (ArithmeticException e) {
 
             System.out.println(e.getMessage());
 
+        } catch (Exception e) {
+
+            System.out.println("Catches a normal exception.");
+
+        } finally {
+
+            System.out.println("This will always execute.");
+
         }
 
+    }
+
+    static int divide(int a, int b) throws ArithmeticException {
+
+        if (b == 0) {
+
+            throw new ArithmeticException("Please don't divide by zero.");
+
+        }
+
+        return a / b;
 
     }
 
