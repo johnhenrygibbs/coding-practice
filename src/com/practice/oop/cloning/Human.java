809b4a4b7a543956233bb487916b216a617fe821
John Gibbs <hello@johnhenrygibbs.com>
2024-04-03 18:17:00 -0700

    Shallow + Deep Copying

--- src/com/practice/oop/cloning/Human.java
+++ src/com/practice/oop/cloning/Human.java
@@ -4,21 +4,30 @@ public class Human implements Cloneable {
 
     int age;
     String name;
+    int[] arr;
 
     public Human(int age, String name) {
         this.age = age;
         this.name = name;
-    }
-
-    public Human(Human other) {
-        this.age = other.age;
-        this.name = other.name;
+        this.arr = new int[]{3, 4, 5, 6, 9, 1};
     }
 
     @Override
     public Object clone() throws CloneNotSupportedException {
 
-        return super.clone();
+        // This is shallow copy.
+        Human twin = (Human)super.clone();
+
+        // This is how you make a deep copy.
+        twin.arr = new int[twin.arr.length];
+
+        for (int i = 0; i < twin.arr.length; i++) {
+
+            twin.arr[i] = this.arr[i];
+
+        }
+
+        return twin;
 
     }
 
--- src/com/practice/oop/cloning/Main.java
+++ src/com/practice/oop/cloning/Main.java
@@ -1,5 +1,7 @@
 package com.practice.oop.cloning;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) throws CloneNotSupportedException {
@@ -7,7 +9,14 @@ public class Main {
         Human john = new Human(34, "John Gibbs");
 
         Human twin = (Human)john.clone();
+
         System.out.println(twin.age + " " + twin.name);
+        System.out.println(Arrays.toString(twin.arr));
+
+        twin.arr[0] = 100;
+
+        System.out.println(Arrays.toString(twin.arr));
+        System.out.println(Arrays.toString(john.arr));
 
     }
 
