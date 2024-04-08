aad5c9c1d0888e70e00a14338bc8a1fbca0d744d
John Gibbs <hello@johnhenrygibbs.com>
2024-04-07 22:43:57 -0700

    String Creation

---
+++ src/com/practice/oop/strings/Main.java
@@ -0,0 +1,14 @@
+package com.practice.oop.strings;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 3, 4};
+        int a = 10;
+        String name = "John Gibbs";
+        System.out.println(name);
+
+    }
+
+}
