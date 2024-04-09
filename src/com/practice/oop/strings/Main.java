eabe07a09c1ad4889e525b971378f8ef336960d6
John Gibbs <hello@johnhenrygibbs.com>
2024-04-08 22:45:23 -0700

    String Immutability

--- src/com/practice/oop/strings/Main.java
+++ src/com/practice/oop/strings/Main.java
@@ -5,9 +5,19 @@ public class Main {
     public static void main(String[] args) {
 
         int[] arr = {2, 3, 4};
-        int a = 10;
+        int num = 10;
         String name = "John Gibbs";
         System.out.println(name);
+
+        // Strings are immutable for security reasons.
+
+        String myName = "John";
+        System.out.println(a);
+
+        myName = "Cassie";
+        System.out.println(a);
+
+        // In this example, myName has not been changed, but rather another object ("Cassie") has been made in the heap.
 
     }
 
