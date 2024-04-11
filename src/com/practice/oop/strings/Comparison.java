479e981ca84bfc269d23628929d2507233aefc58
John Gibbs <hello@johnhenrygibbs.com>
2024-04-10 21:54:41 -0700

    String Comparison .equals()

--- src/com/practice/oop/strings/Comparison.java
+++ src/com/practice/oop/strings/Comparison.java
@@ -17,6 +17,8 @@ public class Comparison {
         String name2 = new String("John");
 
         System.out.println(name1 == name2);
+        System.out.println(name1.equals(name2));
+        System.out.println(name1.charAt(0));
 
     }
 
