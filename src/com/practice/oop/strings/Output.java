ca991b97619364f5f69fcd3f57a908fe2e392900
John Gibbs <hello@johnhenrygibbs.com>
2024-04-11 22:01:18 -0700

    PrintStream Class

---
+++ src/com/practice/oop/strings/Output.java
@@ -0,0 +1,16 @@
+package com.practice.oop.strings;
+
+public class Output {
+
+    public static void main(String[] args) {
+
+        System.out.println(56);
+        System.out.println("John");
+        System.out.println(new int[]{1, 2, 3, 4});
+
+        String name = null;
+        System.out.println(name);
+
+    }
+
+}
