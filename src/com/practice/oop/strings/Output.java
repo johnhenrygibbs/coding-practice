85df8c2903437036efee9d107c00eee87d6f4895
John Gibbs <hello@johnhenrygibbs.com>
2024-04-12 22:11:49 -0700

    toString() Method

--- src/com/practice/oop/strings/Output.java
+++ src/com/practice/oop/strings/Output.java
@@ -1,12 +1,18 @@
 package com.practice.oop.strings;
 
+import java.util.Arrays;
+
 public class Output {
 
     public static void main(String[] args) {
 
+        Integer num = new Integer(56);
+        System.out.println(num.toString());
+        System.out.println(num);
+
         System.out.println(56);
         System.out.println("John");
-        System.out.println(new int[]{1, 2, 3, 4});
+        System.out.println(Arrays.toString(new int[]{1, 2, 3, 4}));
 
         String name = null;
         System.out.println(name);
