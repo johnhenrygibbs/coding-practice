d10981eb6d528b56ebe70b733edffcb8dbbae5e6
John Gibbs <hello@johnhenrygibbs.com>
2024-04-13 22:18:23 -0700

    Pretty Printing

---
+++ src/com/practice/oop/strings/PrettyPrinting.java
@@ -0,0 +1,16 @@
+package com.practice.oop.strings;
+
+public class PrettyPrinting {
+
+    public static void main(String[] args) {
+
+        float a = 432.2341f;
+        System.out.printf("Formatted number is %.2f", a);
+        System.out.println("\n");
+        System.out.printf("Pie: %.3f", Math.PI);
+        System.out.println("\n");
+            System.out.printf("Hello my name is %s and I am %s", "PLACE", "HOLDER");
+
+    }
+
+}
