ab277fab608fa915d92f72d3c56a35a0e55ed433
John Gibbs <hello@johnhenrygibbs.com>
2024-04-16 21:48:16 -0700

    String Concatenation Operator

--- src/com/practice/oop/strings/Operators.java
+++ src/com/practice/oop/strings/Operators.java
@@ -5,6 +5,8 @@ public class Operators {
     public static void main(String[] args) {
 
         System.out.println('a' + 'b');
+        System.out.println("a" + "b");
+        System.out.println((char)('a' + 3));
 
     }
 
