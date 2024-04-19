14dd0a5ae910072e95ed0a1821d5fa6efb47c265
John Gibbs <hello@johnhenrygibbs.com>
2024-04-18 21:49:07 -0700

    String Performance

---
+++ src/com/practice/oop/strings/Performance.java
@@ -0,0 +1,21 @@
+package com.practice.oop.strings;
+
+public class Performance {
+
+    public static void main(String[] args) {
+
+        String series = "";
+
+        for (int i = 0 ; i < 26; i++) {
+
+            char ch = (char)('a' + i);
+
+            series += ch;
+
+        }
+
+        System.out.println(series);
+
+    }
+
+}
