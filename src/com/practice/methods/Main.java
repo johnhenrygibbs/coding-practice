63876347e1a195e85ecb089423d6d9e28fa1e973
John Gibbs <hello@johnhenrygibbs.com>
2024-04-28 19:18:41 -0700

    Problem Statement

---
+++ src/com/practice/methods/Main.java
@@ -0,0 +1,23 @@
+package com.practice.methods;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Take an input of two numbers and print the sum.
+
+        Scanner in = new Scanner(System.in);
+        int numOne, numTwo, sum;
+
+        System.out.print("Enter No. 1: ");
+        numOne = in.nextInt();
+        System.out.print("Enter No. 2: ");
+        numTwo = in.nextInt();
+        sum = numOne + numTwo;
+        System.out.println("The sum = " + sum);
+
+    }
+
+}
