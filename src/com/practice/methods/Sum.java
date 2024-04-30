cf983820fc1fd5a2cfa5df236c75d75d13d2b969
John Gibbs <hello@johnhenrygibbs.com>
2024-04-29 18:32:24 -0700

    Method Syntax

---
+++ src/com/practice/methods/Sum.java
@@ -0,0 +1,22 @@
+package com.practice.methods;
+
+public class Sum {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    // Below is the syntax of a method.
+
+    /*
+
+        access modifier, return type, name () {
+            // body
+            return statement;
+        }
+
+    */
+
+}
