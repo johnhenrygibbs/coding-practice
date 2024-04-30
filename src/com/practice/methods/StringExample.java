0a63540b7ce4c36e0e41eaa404c476474d9de7ac
John Gibbs <hello@johnhenrygibbs.com>
2024-04-30 13:36:02 -0700

    Returning a String

---
+++ src/com/practice/methods/StringExample.java
@@ -0,0 +1,21 @@
+package com.practice.methods;
+
+public class StringExample {
+
+    public static void main(String[] args) {
+
+        String message = greet();
+        System.out.println(message);
+
+    }
+
+    static String greet() {
+
+        String greeting = "How are you?";
+
+        return greeting;
+
+    }
+
+
+}
