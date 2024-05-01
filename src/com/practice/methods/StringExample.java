62f55d6f54399de8ca4987d18625b055dacad777
John Gibbs <hello@johnhenrygibbs.com>
2024-05-01 09:17:43 -0700

    Parameters (String Function)

--- src/com/practice/methods/StringExample.java
+++ src/com/practice/methods/StringExample.java
@@ -1,5 +1,7 @@
 package com.practice.methods;
 
+import java.util.Scanner;
+
 public class StringExample {
 
     public static void main(String[] args) {
@@ -7,7 +9,21 @@ public class StringExample {
         String message = greet();
         System.out.println(message);
 
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = in.next();
+        String uniqueMessage = myGreeting(name);
+        System.out.println(uniqueMessage);
+
     }
+
+    private static String myGreeting(String name) {
+
+        String message = "Hello, " + name;
+        return message;
+
+    }
+
 
     static String greet() {
 
